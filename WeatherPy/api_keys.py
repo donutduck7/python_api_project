1aceae25fd0b85950ce06f4a80a986c7d4f72ba3
Tasnia Wahid <137854789+tasniawahid7@users.noreply.github.com>
2023-11-29 19:16:48 -0500

    Add files via upload

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "8f146f7bd90450d1d465794d4ef8f8fa"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "2c44fef549a1435cad30c64d24d5411a"
